11f6fcf0d80f8cc1ddba4544891849dc2267bc96
sharmavimal7467 <139248526+sharmavimal7467@users.noreply.github.com>
2023-09-28 15:42:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
-
+let regexp = /\bthe\b|\ban\b|\ba\b/gi;
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let touristSpotsArticleLessArray = [];
+let mp = {};
+for(let i = 0;i<touristSpots.length;i++){
+	let touristSpotsItem = touristSpots[i].replace(regexp,"").trim();
+	touristSpotsArticleLessArray.push(touristSpotsItem);
+	mp[touristSpotsItem] = touristSpots[i];
+}
+touristSpotsArticleLessArray.sort();
+let ans = [];
+for(let i of touristSpotsArticleLessArray){
+	ans.push(i);
+}
+console.log(ans);
